1ed4d570fe24121c1c23be295157595f5568e02f
Mohamed Ziad <mziad@banquemisr.com>
2025-12-11 19:10:07 +0200

    Initial Commit

---
+++ VocabularyTests/VocabularyTests.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyTests.swift
+//  VocabularyTests
+//
+//  Created by Mohamed Ziad on 11/12/2025.
+//
+
+import Testing
+@testable import Vocabulary
+
+struct VocabularyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
